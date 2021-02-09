f3cffba08003a7395f7b1936d13240a3c801d575
zhu <zoeleeeeeeee@gmail.com>
2021-02-09 20:25:10 +0800

    .

--- mine/model.py
+++ mine/model.py
@@ -1,45 +1,161 @@
-from dataset import encode
-from distance import hamming, euclidean
-import torch 
+'''ResNet in PyTorch.
+For Pre-activation ResNet, see 'preact_resnet.py'.
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from robustness.tools.custom_modules import SequentialWithArgs, FakeReLU
+from dataset import rand_perm
 
-class ImageModel():
-    def __init__(self, model_name, dataset_name, train = False, load = False, **kwargs):
-        self.model_name = model_name
-        self.dataset_name = dataset_name
-        self.data_model = dataset_name + model_name
-        self.framework = 'pytorch'
-        self.num_classes = 10
+class BasicBlock(nn.Module):
+    expansion = 1
 
-        print('Load network...')
-        self.nb_model = 3
-        model_file_form = '../cifar_update/models/cifar10/256.32_cifar10_5_{}.pt'
-        self.models = []
-        for i in range(self.nb_model):
-            model = torch.load(model_file_form.format(i))
-            model = torch.nn.DataParallel(model).cuda()
-            self.models.append(model.eval())
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-        self.label_reps = []
-        for i in range(self.nb_model):
-            np.random.seed(i*5)
-            self.label_reps.append(np.random.permutation(np.load('data/5_label_permutation.npy')))
-        self.label_reps = np.hstack(self.label_reps)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1,
+                          stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes))
 
-    def predict(self, x, y=0, metric='hamming'):
-        assert metric in ['hamming', 'euclidean']
+    def forward(self, x, fake_relu=False):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        if fake_relu:
+            return FakeReLU.apply(out)
+        return F.relu(out)
 
-        if np.max(x) <= 1:
-            x = (x*255).astype(np.uint8)
-        if len(x.shape) == 3:
-            x = np.expand_dims(x, axis=0)
-        assert len(x.shape)==4, 'x shape {} error'.format(x.shape)
 
-        scores = []
-        for i in range(len(self.models)):
-            xx = encode(x, i, 32, 0)
-            scores.append(self.models[i](torch.Tensor(xx).cuda(), 1).detach().cpu().numpy())
-        scores = np.hstack(scores)
+class Bottleneck(nn.Module):
+    expansion = 4
 
-        if metric == 'hamming': dists, _, _ = hamming(scores, 0.9, self.label_reps, y)
-        elif metric == 'euclidean': dists, _, _ = euclidean(scores, 0.9, self.label_reps, y)
-        return np.argmin(dists[0])
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+      
+        self.shortcut = nn.Sequential()
+        self.dropout = nn.Dropout(0.7)
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x, fake_relu=False):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        if fake_relu:
+            return FakeReLU.apply(out)
+        return F.relu(out)
+
+
+class ResNet(nn.Module):
+    # feat_scale lets us deal with CelebA, other non-32x32 datasets
+    def __init__(self, block, num_blocks, num_classes=10, feat_scale=1, wm=1, nb_channal=3):
+        super(ResNet, self).__init__()
+
+        widths = [64, 128, 256, 512]
+        widths = [int(w * wm) for w in widths]
+
+        self.in_planes = widths[0]
+        self.conv1 = nn.Conv2d(nb_channal, self.in_planes, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.layer1 = self._make_layer(block, widths[0], num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, widths[1], num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, widths[2], num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, widths[3], num_blocks[3], stride=2)
+        self.clinear = nn.Linear(feat_scale*widths[3]*block.expansion, num_classes[0])
+        self.dlinear = nn.Linear(feat_scale*widths[3]*block.expansion, num_classes[1])
+        self.dropout  = nn.Dropout(0.5)
+        self.dropout_cnn  = nn.Dropout(0.25)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return SequentialWithArgs(*layers)
+
+    def forward(self, x, sign, with_latent=False, fake_relu=False, no_relu=False, permutation=None, p=None):
+        assert (not no_relu),  \
+            "no_relu not yet supported for this architecture"
+        if p is not None: x = rand_perm(x, permutation, p)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.dropout_cnn(out)
+        out = self.layer2(out)
+        out = self.dropout_cnn(out)
+        out = self.layer3(out)
+        out = self.dropout_cnn(out)
+        out = self.layer4(out, fake_relu=fake_relu)
+        out = F.avg_pool2d(out, 4)
+        out = self.dropout_cnn(out)
+        pre_out = out.view(out.size(0), -1)
+        if sign == 0:
+            final = self.clinear(pre_out)
+        elif sign == 1:
+            final = self.dlinear(pre_out)
+        if with_latent:
+            return final, pre_out
+        return final
+
+def ResNet18(nb_channal=3, **kwargs):
+    return ResNet(BasicBlock, [2,2,2,2], nb_channal=nb_channal, **kwargs)
+
+def ResNet18Wide(**kwargs):
+    return ResNet(BasicBlock, [2,2,2,2], wm=5, **kwargs)
+
+def ResNet18Thin(**kwargs):
+    return ResNet(BasicBlock, [2,2,2,2], wm=.5, **kwargs)
+
+def ResNet34(nb_channal=3, **kwargs):
+    return ResNet(BasicBlock, [3,4,6,3], nb_channal=nb_channal, **kwargs)
+
+def ResNet50(nb_channal=10, **kwargs):
+    return ResNet(Bottleneck, [3,4,6,3], nb_channal=nb_channal, **kwargs)
+
+def ResNet50Wide(nb_channal=10, **kwargs):
+    return ResNet(Bottleneck, [3,4,6,3], wm=5, **kwargs)
+
+def ResNet101(**kwargs):
+    return ResNet(Bottleneck, [3,4,23,3], **kwargs)
+
+def ResNet152(**kwargs):
+    return ResNet(Bottleneck, [3,8,36,3], **kwargs)
+
+resnet50 = ResNet50
+resnet18 = ResNet18
+resnet18thin  = ResNet18Thin
+resnet34 = ResNet34
+resnet101 = ResNet101
+resnet152 = ResNet152
+resnet18wide = ResNet18Wide
+resnet50wide = ResNet50Wide
+
+# resnet18thin = ResNet18Thin
+def test():
+    net = ResNet18()
+    y = net(torch.randn(1,3,32,32))
+    print(y.size())
