a50feddbaa052c59a66ef6f7d13ae0a3c47cbe15
zhu <zoeleeeeeeee@gmail.com>
2021-02-25 18:14:38 +0800

    distance to evaluate hsja results

--- distance.py
+++ distance.py
@@ -1,25 +1,30 @@
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
 
 file = sys.argv[-1]
 with open(file) as f:
-	cnt = f.readlines()
+    cnt = f.readlines()
 
 preline = None
 dists = []
 for line in cnt:
-	if line.endswith('sample...') and preline is not None:
-		dists.append(float(line.split(':')[-1].split()[-1]))
+    if line.endswith('sample...') 
+        if preline is not None:
+            dists.append(float(line.split(':')[-1].split()[-1]))
+        else: idx = int(line.split()[-2])
+    preline = line
+    if idx = 100: break
 
 ths = np.linspace(0,10, 1000)
 amt = []
 dists = np.array(sorted(dists))
 for i in ths:
-	idx = np.argmin(np.abs(dists-i))
-	if dists[idx] < i: idx += 1
-	if i == 0: amt.append(idx)
-	else: amt.append(idx-amt[-1])
-	if idx == 99: break
+    idx = np.argmin(np.abs(dists-i))
+    if dists[idx] < i: idx += 1
+    if i == 0: amt.append(idx)
+    else: amt.append(idx-amt[-1])
+    if idx == 99: break
 
 plt.bar(ths[:len(amt)], amt)
 plt.savefig('res/{}.png'.format(file[:-4]))
