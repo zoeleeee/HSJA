408240092e2b1e891677355a466f62bb7922b23a
zhu <zoeleeeeeeee@gmail.com>
2021-02-25 18:10:37 +0800

    .

---
+++ distance.py
@@ -0,0 +1,25 @@
+import sys
+import matplotlib.pyplot as plt
+
+file = sys.argv[-1]
+with open(file) as f:
+	cnt = f.readlines()
+
+preline = None
+dists = []
+for line in cnt:
+	if line.endswith('sample...') and preline is not None:
+		dists.append(float(line.split(':')[-1].split()[-1]))
+
+ths = np.linspace(0,10, 1000)
+amt = []
+dists = np.array(sorted(dists))
+for i in ths:
+	idx = np.argmin(np.abs(dists-i))
+	if dists[idx] < i: idx += 1
+	if i == 0: amt.append(idx)
+	else: amt.append(idx-amt[-1])
+	if idx == 99: break
+
+plt.bar(ths[:len(amt)], amt)
+plt.savefig('res/{}.png'.format(file[:-4]))
